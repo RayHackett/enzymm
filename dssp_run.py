b27cf023642ea63df16fe540f8e86f97ec9830c2
Raymund Hackett <r.e.hackett@lumc.nl>
2024-01-07 13:16:25 +0000

    adapted dssp_run.py for unknown pdb structures

--- dssp_run.py
+++ dssp_run.py
@@ -1,6 +1,6 @@
 """
 written by Ray
-script: dssp_run.py
+script: dssp_run_unknown_structure.py
 this is called by batch_dssp.py: 
 script to run dssp against a list of input structues and print the relative solvent accessibility to the occupancy column in the output pdb structure
 
@@ -48,23 +48,8 @@ cwd = Path.cwd()
 results_path = Path(cwd,'dssp_results') 
 results_path.mkdir(exist_ok=True)
 
-pdb_path = Path(cwd,'cifchains') 
+pdb_path = Path(cwd,'structure_chains') 
 pdb_path.mkdir(exist_ok=True)
-
-with open('/homes/hackett/catalytic_residues_homologues.json', 'r') as f:
-    homologs = json.load(f)
-
-pdb_chain_dict = {}
-for i in homologs:
-    for res in i['residue_chains']:
-        #if res['assembly'] != None: # we ignore the ones with assembly == none
-        name = res['pdb_id']
-        if name not in pdb_chain_dict:
-            pdb_chain_dict[name] = set()
-        if res['is_reference']:
-            pdb_chain_dict[name].add(res['assembly_chain_name'])
-        else:
-            pdb_chain_dict[name].add(res['chain_name'])
 
 with open(pdb_file, 'r') as f:
     lines = f.readlines()
@@ -79,18 +64,21 @@ class ChainSelect(Select):
     def accept_chain(self, chain):
         if chain.get_id() == self.chain:
             return 1
-        else:          
+        else:
             return 0
 
 dssp_successes = []
 for pdb in pdb_list:
     dssp_path = '/hps/software/users/thornton/hackett/anaconda3/envs/mod_biopy/bin/mkdssp'
     try:
-        #structure = PDBParser(QUIET=True).get_structure(str(Path(pdb).name), Path(pdb))
-        structure = MMCIFParser(QUIET=True).get_structure(str(Path(pdb).name), Path(pdb))
-        # we get the correct chain from the homologs dictionary
-        chains = list(pdb_chain_dict[str(Path(pdb).stem)[:4].lower()])
-    
+        if Path(pdb).suffix.upper()[1:] == 'PDB':
+            structure = PDBParser(QUIET=True).get_structure(str(Path(pdb).name), Path(pdb))
+        elif Path(pdb).suffix.upper()[1:] == 'CIF':
+            structure = MMCIFParser(QUIET=True).get_structure(str(Path(pdb).name), Path(pdb))
+        else:
+            print('Please add a recognizable file extension like .pdb or .cif')
+            continue
+
         model = structure[0] # always only one model per pdb file since we are dealing with predicted structures
         dssp = DSSP(model, Path(pdb), dssp=dssp_path, acc_array='Miller') # Miller since the Group useses that
         dssp_dict = dict(dssp)
@@ -100,33 +88,38 @@ for pdb in pdb_list:
         # (dssp index, amino acid, secondary structure, relative ASA, phi, psi,
         # NH_O_1_relidx, NH_O_1_energy, O_NH_1_relidx, O_NH_1_energy,
         # NH_O_2_relidx, NH_O_2_energy, O_NH_2_relidx, O_NH_2_energy)
-    
+
         # add the relative solvent accessibility (normalized with Miller) to the occupancy column in the structure
         for i, residue in enumerate(structure.get_residues()):
-                # check if it is a value
-                data = dssp_dict.get((residue.get_full_id()[2], (' ', residue.id[1], residue.id[2])))
-                if data and isinstance(data[3], float): # incomplete residues are not evaluated, HETATM get 'NA'
-                    rsa = float(data[3])
-                    #ss = data[2] # secondary structure
-                else:
-                    rsa = 9.99
-                #plddt = residue['CA'].get_bfactor() / 100.0 # plddt
-                atoms = residue.get_atoms()
-                for atom in atoms:
-                    atom.set_occupancy(rsa)
-                    
-        # save the modified structure to the same file location
-        for chain in chains:
-            pdb_chain_file = str(Path(pdb_path,str(Path(pdb).stem[:4].upper() + chain + '.pdb')))
-            io=PDBIO(for_jess=True)
-            io.set_structure(structure)
-            io.save(pdb_chain_file, ChainSelect(chain), preserve_atom_numbering=True)
-            with open(pdb_chain_file, 'r') as f:
-                content = f.read()
-            if content.strip() == 'END':
-                Path(pdb_chain_file).unlink()
+            # check if it is a value
+            data = dssp_dict.get((residue.get_full_id()[2], (' ', residue.id[1], residue.id[2])))
+            if data and isinstance(data[3], float): # incomplete residues are not evaluated, HETATM get 'NA'
+                rsa = float(data[3])
+                #ss = data[2] # secondary structure
             else:
+                rsa = 9.99
+            #plddt = residue['CA'].get_bfactor() / 100.0 # plddt
+            atoms = residue.get_atoms()
+            for atom in atoms:
+                atom.set_occupancy(rsa)
+
+        # save the modified structure to the same file location
+        # a structure consists of models which consist of chain(s)
+        for chain in model:
+            atoms = chain.get_atoms()
+            # Check if the iterator is empty or not
+            print(atoms)
+            try:
+                next(atoms)
+                pdb_chain_file = str(Path(pdb_path, str(Path(pdb).stem) + str(chain.id) + '.pdb'))
+                #io=PDBIO(for_jess=True) # this removes some columns including the pLDDT column
+                io=PDBIO()
+                io.set_structure(structure)
+                #io.save(pdb_chain_file, ChainSelect(chain), preserve_atom_numbering=True)
+                io.save(pdb_chain_file, preserve_atom_numbering=True)
                 dssp_successes.append(pdb_chain_file)
+            except StopIteration:
+                print("The structure object is empty.")
         
     except Exception:
         traceback.print_exc()
@@ -134,4 +127,3 @@ for pdb in pdb_list:
 with open(Path(results_path, Path(pdb_file).name.split('.')[0] + '.list'), 'w') as f:
     for i in dssp_successes:
         f.write(str(i) + '\n')
-    
